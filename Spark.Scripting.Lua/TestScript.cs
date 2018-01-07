b4ef4b956601bc4c422d4a9e1ebe4a6dcc18c196
Mattiemus <mattie9875@hotmail.co.uk>
2018-01-07 14:05:03 +0000

    Toying around with Lua scripting

---
+++ Spark.Scripting.Lua/TestScript.cs
@@ -0,0 +1,35 @@
+﻿namespace Spark.Scripting.Lua
+{
+    using System;
+
+    using MoonSharp.Interpreter;
+
+    public sealed class TestScript
+    {
+        public static void Test()
+        {
+            string script = @"
+		        -- defines a factorial function
+		        function fact (n)
+			        if (n == 0) then
+				        return 1
+			        else
+				        return n*fact(n - 1)
+			        end
+		        end
+
+	            testing(fact)";
+
+            var test = new Script(CoreModules.None);
+            test.Globals["testing"] = (Func<Closure, int>)Testing;
+
+            DynValue res = test.DoString(script);
+        }
+
+        public static int Testing(Closure func)
+        {
+            var test = func.Call(5);
+            return 5;
+        }
+    }
+}
